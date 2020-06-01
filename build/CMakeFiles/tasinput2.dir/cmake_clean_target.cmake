f215102069ced6062a7584bfd8a780404c6bbf94
not_a_seagull <notaseagull048@gmail.com>
2020-06-01 15:06:09 -0700

    Remove build files

--- build/CMakeFiles/tasinput2.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtasinput2.a"
-)
--- build/CMakeFiles/tasinput2_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tasinput2_autogen"
-  "tasinput2_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tasinput2_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
